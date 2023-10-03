e48e2afdeb5b20b273fa7869b7ea6eec3415268f
MarkoBakac <markobakac123@gmail.com>
2023-10-03 22:41:35 +0200

    Add unit tests for search functionality
    Test valid queries with expected JSON responses.
    Test invalid queries with appropriate error responses.
    Test for the correct count of fetched results.

--- ShowFinder/tests/Feature/SearchControllerTest.php
+++ ShowFinder/tests/Feature/SearchControllerTest.php
@@ -8,13 +8,21 @@ use Tests\TestCase;
 
 class SearchControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function testValidQueryReturns200AndJsonResponse()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/search?q=deadwood');
 
         $response->assertStatus(200);
+
+        $jsonResponse = $response->json();
+
+        // Perform assertions on the JSON data
+        $this->assertArrayHasKey('results', $jsonResponse);
+        $this->assertIsArray($jsonResponse['results']);
+
+        // Check the count of results
+        $this->assertCount(1, $jsonResponse['results']);
     }
 }
+
+
