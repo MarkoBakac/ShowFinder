b9fb68bf5b4e58e892f01451bdabf4b053fcd2b2
MarkoBakac <markobakac123@gmail.com>
2023-10-03 21:53:32 +0200

    Implemented TV search logic
    Request to TVMaze API. Filter shows for exact match. Handle API request.

--- ShowFinder/app/Http/Controllers/SearchController.php
+++ ShowFinder/app/Http/Controllers/SearchController.php
@@ -3,8 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class SearchController extends Controller
 {
-    //
+    public function search(Request $request)
+    {
+        $query = strtolower($request->input('q'));
+
+        // HTTP GET request
+        $response = Http::get('http://api.tvmaze.com/search/shows', [
+            'q' => $query,
+        ]);
+
+        if ($response->successful()) {
+            $shows = $response->json();
+
+            // Include only an exact match
+            $exactMatch = null;
+
+            foreach ($shows as $show) {
+                $showName = strtolower($show['show']['name']);
+                if ($showName === $query) {
+                    $exactMatch = [
+                        'id' => $show['show']['id'],
+                        'name' => $show['show']['name'],
+                        'summary' => $show['show']['summary'],
+                        'language' => $show['show']['language'],
+                        'genres' => $show['show']['genres'],
+                        'status' => $show['show']['status'],
+
+                    ];
+                    break;
+                }
+            }
+
+            if ($exactMatch) {
+                // Return results
+                return response()->json(['results' => [$exactMatch]], 200);
+            } else {
+                // If no exact match
+                return response()->json(['error' => 'No exact match found'], 404);
+            }
+        } else {
+            // Handle API request failure
+            return response()->json(['error' => 'Failed to fetch TV show data'], 500);
+        }
+    }
 }
