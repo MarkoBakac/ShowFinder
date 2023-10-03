7c98864a86d8632a4a9d1aea2fad320eac79d065
MarkoBakac <markobakac123@gmail.com>
2023-10-03 21:43:45 +0200

    Add search route to SearchController

--- ShowFinder/routes/api.php
+++ ShowFinder/routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/search', 'App\Http\Controllers\SearchController@search');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
